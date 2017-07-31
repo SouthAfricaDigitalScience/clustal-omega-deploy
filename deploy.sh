af92b681b33322fc01065129d5948f3b3b7723ee
Bruce Becker <brucellino@gmail.com>
2017-07-31 12:05:22 +0200

    Fix modulefile generation

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,15 @@ whoami
 echo ${SOFT_DIR}
 module add deploy
 echo ${SOFT_DIR}
-cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+cd ${WORKSPACE}/${NAME}-${VERSION}
 echo "All tests have passed, will now build into ${SOFT_DIR}"
-../configure ABI=64 \
+./configure \
 --with-gnu-ld \
 --enable-shared \
---prefix=${SOFT_DIR}
-make install -j2
+--enable-static \
+--prefix=${SOFT_DIR}-gcc-${GCC_VERSION}
+make install
+
 echo "Creating the modules file directory ${LIBRARIES}"
 mkdir -p ${LIBRARIES}/${NAME}
 (
@@ -25,12 +27,12 @@ proc ModulesHelp { } {
     puts stderr "       that the [module-info name] module is not available"
 }
 
-module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/gmp-deploy"
-setenv GMP_VERSION       $VERSION
-setenv GMP_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
-prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
-prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
-prepend-path CFLAGS            "-I$::env(GMP_DIR)/include"
-prepend-path LDFLAGS           "-L$::env(GMP_DIR)/lib"
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/CLUSTAL_OMEGA-deploy"
+setenv CLUSTAL_OMEGA_VERSION       $VERSION
+setenv CLUSTAL_OMEGA_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path PATH                           $::env(CLUSTAL_OMEGA_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(CLUSTAL_OMEGA_DIR)/lib
+prepend-path CFLAGS            "$CFLAGS -I$::env(CLUSTAL_OMEGA_DIR)/include"
+prepend-path LDFLAGS           "$LDFLAGS -L$::env(CLUSTAL_OMEGA_DIR)/lib"
 MODULE_FILE
 ) > ${LIBRARIES}/${NAME}/${VERSION}
